12828fbd829b093f5cd4c7643a7099ed8b31115e
Omar Patel <omarpatel123@gmail.com>
2016-08-18 11:46:08 -0700

    Change authentication to be more manual and currently attempting to decouple authentication from other logic

--- public/js/creds.js
+++ public/js/creds.js
@@ -1,9 +1,57 @@
 // Initialize Firebase
-var config = {
-	apiKey: "AIzaSyDTDMsuvkR461lrFxIahJCAK0_i0RI8MBw",
-	authDomain: "monstergame-9944d.firebaseapp.com",
-	databaseURL: "https://monstergame-9944d.firebaseio.com",
-	storageBucket: "",
-};
 
-firebase.initializeApp(config);
+const $email = $("#email_input");
+const $password = $("#password_input");
+const $signIn= $("#btnSignIn");
+const $signOut = $("#btnSignOut");
+const $signUp = $("#btnSignUp");
+
+$signIn.click(function(){
+	const email = $email.val();
+	const password = $password.val();
+	const auth = firebase.auth();
+
+	const promise = auth.signInWithEmailAndPassword(email, password);
+	promise.catch(function(e){
+		console.log(e.message)
+	});
+});
+
+$signOut.click(function(){
+	firebase.auth().signOut();
+});
+
+$signUp.click(function(){
+	// Create some validation for emails
+	const email = $email.val();
+	const password = $password.val();
+	const auth = firebase.auth();
+
+	const promise = auth.createUserWithEmailAndPassword(email, password);
+	promise.catch(function(e){
+		console.log(e.message);
+	});
+});
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    user.getToken().then(function(accessToken) {
+
+    	afterAuth(user);
+    	
+    	// Display log in name
+    	signedInDisplay(user.displayName);
+
+    	// for (var i = 0; i < 100; i++) {
+    	// 	addRoomAndEmptySeats();
+    	// }
+  });
+  } else {
+    // User is signed out.
+    signedOutDisplay(user.displayName);
+  }
+}, function(error) {
+  console.log(error);
+});
--- public/js/logic.js
+++ public/js/logic.js
@@ -1,253 +1,234 @@
-$( document ).ready(function() {
 
+// Set up refs for Database endpoints
+var db = firebase.database();
+var playersRef = db.ref('/players');
+var roomsRef = db.ref('/channels');
+var amOnline = db.ref('/.info/connected');
+var playerChoiceHTML = false;
 
-	// Set up refs for Database endpoints
-	var db = firebase.database();
-	var playersRef = db.ref('/players');
-	var roomsRef = db.ref('/channels');
-	var amOnline = db.ref('/.info/connected');
-	var playerChoiceHTML = false;
+function addRoomAndEmptySeats() {
 
-	function addRoomAndEmptySeats() {
-
-	  var newRoom = {
-	  	seat1: "empty",
-	  	seat2: "empty",
-	  	seat3: "empty",
-	  	seat4: "empty",
-	  	locked: false,
-	  	numPlayers: 0
-	  }
-
-	  roomsRef.push(newRoom);
-	}
-
-	$(document).on('click', '.chooseCharacter', function() {
-
-	  
-	});
-
-	function signedInDisplay(displayName) {
-		$("#firebaseui-auth-container").hide();
-		$("#firebase_authentication").html("<div class='col-sm-12'>" + "<h1>" + displayName + " is signed in.</h1>" +
-        "</div>");
-	}
-
-	function signedOutDisplay(theUser) {
-		$("#firebaseui-auth-container").show();
-		$("#firebase_authentication").html("<div class='col-sm-12'>" + "<h1>" + displayName + " is signed out.</h1>" +
-        "</div>");
-	}
-
-	// Note, this returns a promise
-	function findPlayer(uid) {
-  	return playersRef.once("value")
-  		.then(function(snapshot) {
-  			var result = false;
-  			snapshot.forEach(function(childSnapshot) {
-	  			if (childSnapshot.val().uid === uid) {
-					result = childSnapshot.val();
-				}
-			});
-			return result;
-  		});
+  var newRoom = {
+  	seat1: "empty",
+  	seat2: "empty",
+  	seat3: "empty",
+  	seat4: "empty",
+  	locked: false,
+  	numPlayers: 0
   }
 
-  // This will return a promise
-  function checkIfPlayerExists(uid) {
-  	return playersRef.once("value")
-  		.then(function(snapshot) {
-  			var result = [false];
-  			snapshot.forEach(function(childSnapshot) {
-	  			if (childSnapshot.val().uid === uid) {
-					console.log("We found it!");
-					result[0] = true;
-					result.push(childSnapshot.val());
-				}
-			});
-			return result;
-  	});
-  }
+  roomsRef.push(newRoom);
+}
 
-  function chooseCharacterPageLoad() {
-  	sessionStorage.setItem("characterScreenLoaded", "true");
-		window.location = "../chooseCharacter.html";
-  }
+$(document).on('click', '.chooseCharacter', function() {
 
-  function chooseCharacter(uid, playerObj) {
-  		// Do all the stuff for choosing a character. You should now be on choose character page
-  }
-
-  function seatCheckAndRoomUpdate(uid, thePlayerObject){
-
-		return roomsRef.once("value")
-		 	.then(function(snapshot) {
-
-		 		var arrayOfData = [thePlayerObject];
-		 		var dataHasBeenSet = false;
-
-		 		// look through each room and find one that is empty
-		  	snapshot.forEach(function(childSnapshot) {
-		  		childSnapshot.forEach(function(roomProp) {
-		  			
-			  			if (roomProp.val() == "empty" && thePlayerObject.room == null) {
-
-			  				var roomKey = childSnapshot.key;
-			  				var nameOfRoom = roomProp.key;
-			  				var numberOfPlayers = childSnapshot.val().numPlayers;
-
-								// find Player and assign room key to the room property
-							  // Make this more efficient by putting code into a function
-							  playersRef.once('value', function(snapshot) {
-							  	snapshot.forEach(function(childSnapshot) {
-							  		if (childSnapshot.val().uid === uid) {
-							  			childSnapshot.ref.update({ 
-									  		room: roomKey 
-									  	});
-							  		}
-							  	})
-							  }).then(function(result){
-							  	roomProp.ref.set(uid);
-							  })
-								
-							  // Now simply find the parent and return the name.
-							  if (numberOfPlayers < 4) {
-							  	childSnapshot.ref.update({ 
-							  		numPlayers: numberOfPlayers + 1 
-							  	});
-							  // Make room locked if there are a total of 4 players
-							  } else if (numberOfPlayers == 4) {
-							  	childSnapshot.ref.update({ locked: true });
-							  }
-
-							  chooseCharacterPageLoad();
-							  
-							 	objData.key = childSnapshot.key;
-							 	dataHasBeenSet = true;
-							}
-							if (dataHasBeenSet) {
-			  				// will exit out of loop
-			  				return true;
-			  			}
-		  		})
-				});
-				return objData;
-			});
-  }
-
-  function createRoom(uid) {
-  	return playersRef.once("value")
-  		.then(function(snapshot) {
-  			var result = false;
-  			snapshot.forEach(function(childSnapshot) {
-	  			if (childSnapshot.val().uid === uid) {
-					console.log("We found it!");
-					// currentPlayer = childSnapshot.val();
-					result = true;
-				}
-			});
-			return result;
-  		});
-  }
-
-	function createPlayer(uid, displayName, playerExists) {
-
-	  var playerData = {
-	    name: displayName,
-	    uid: uid,
-	    hitPoints: 100, //Default to 100 hitpoints for now
-	    turn: 0, // Default to 0 for now. This could be used later with a data ref to turns/
-	    playerImage: "", //Currently set to some default image. This will be based off of the character the user chooses.
-	    attacks: {
-	    	basicAttack: 35,
-	    	advancedAttack: 50,
-	    	ultraAttack: 75,
-	    	ridiculousAttack: 125
-	    },
-	    items: {},
-	    room: null
-	  };
-
-	  // Get a key for a new Player.
-	  var newPlayerKey = playersRef.push().key;
-
-	  // Write the new player's data in the players list.
-	  var updates = {};
-	  updates['/players/' + newPlayerKey] = playerData;
-
-	  db.ref().update(updates);
-
-	  return playerData;
-	}
-
-	var initApp = function() {
-		firebase.auth().onAuthStateChanged(function(user) {
-	    if (user) {
-	      // User is signed in.
-
-	      user.getToken().then(function(accessToken) {
-
-	      	var userRef = db.ref('/presence/' + user.uid);
-
-		      // checks to see if the user is logged in.
-	      	amOnline.on('value', function(snapshot) {
-					  if (snapshot.val()) {
-					  	// Preferably all users see that user has been disconnected and ill come back.
-					    userRef.onDisconnect().remove();
-
-					    userRef.set(true);
-
-					    // Check to see if player already created
-						  checkIfPlayerExists(user.uid)
-						  	.then(function(arrayOfBooleanAndPlayer){
-
-						  		var playerObj;
-
-						  		// If player doesn't exist, create the player, otherwise mention that player is already in the system
-						  		if (arrayOfBooleanAndPlayer[0] == false) {
-						  			playerObj = createPlayer(user.uid, user.displayName);
-						  		} else if (arrayOfBooleanAndPlayer[0]) {
-						  			playerObj = arrayOfBooleanAndPlayer[1];
-						  		}
-
-						  		console.log(playerObj);
-
-						  		return playerObj;
-
-						  	}).then(function(playerObject){
-						  		// I want the result to be the player that was created
-						  		if (sessionStorage.getItem("characterScreenLoaded") !== 'true') {
-						  			return seatCheckAndRoomUpdate(user.uid, playerObject);
-						  		} else if () {
-
-						  		} else {
-						  			// be sure to return the playerObject so that it gets passed to the next function
-						  			chooseCharacter(user.uid, playerObject);
-						  		}
-						  	}).then(function(result){
-						  			console.log("result from seatCheckAndRoomUpdate", result);
-						  	});
-					  }
-					});
-	      	
-	      	// Display log in name
-	      	signedInDisplay(user.displayName);
-
-	      	// for (var i = 0; i < 100; i++) {
-	      	// 	addRoomAndEmptySeats();
-	      	// }
-	    });
-	    } else {
-	      // User is signed out.
-	      signedOutDisplay(user.displayName);
-	    }
-	  }, function(error) {
-	    console.log(error);
-	  });
-	};
-
-	window.addEventListener('load', function() {
-		initApp();
-	});
-
+  
 });
+
+function signedInDisplay(displayName) {
+	$("#firebaseui-auth-container").hide();
+	$("#firebase_authentication").html("<div class='col-sm-12'>" + "<h1>" + displayName + " is signed in.</h1>" +
+      "</div>");
+}
+
+function signedOutDisplay(theUser) {
+	$("#firebaseui-auth-container").show();
+	$("#firebase_authentication").html("<div class='col-sm-12'>" + "<h1>" + displayName + " is signed out.</h1>" +
+      "</div>");
+}
+
+// Note, this returns a promise
+function findPlayer(uid) {
+	return playersRef.once("value")
+		.then(function(snapshot) {
+			var result = false;
+			snapshot.forEach(function(childSnapshot) {
+  			if (childSnapshot.val().uid === uid) {
+				result = childSnapshot.val();
+			}
+		});
+		return result;
+		});
+}
+
+// This will return a promise
+function checkIfPlayerExists(uid) {
+	return playersRef.once("value")
+		.then(function(snapshot) {
+			var result = [false];
+			snapshot.forEach(function(childSnapshot) {
+  			if (childSnapshot.val().uid === uid) {
+				console.log("We found it!");
+				result[0] = true;
+				result.push(childSnapshot.val());
+			}
+		});
+		return result;
+	});
+}
+
+function chooseCharacterPageLoad() {
+	sessionStorage.setItem("characterScreenLoaded", "true");
+	window.location = "../chooseCharacter.html";
+}
+
+function chooseCharacter(uid, playerObj) {
+		// Do all the stuff for choosing a character. You should now be on choose character page
+}
+
+function seatCheckAndRoomUpdate(uid, thePlayerObject){
+
+	return roomsRef.once("value")
+	 	.then(function(snapshot) {
+
+	 		var arrayOfData = [thePlayerObject];
+	 		var dataHasBeenSet = false;
+
+	 		// look through each room and find one that is empty
+	  	snapshot.forEach(function(childSnapshot) {
+	  		childSnapshot.forEach(function(roomProp) {
+	  			
+		  			if (roomProp.val() == "empty" && thePlayerObject.room == null) {
+
+		  				var roomKey = childSnapshot.key;
+		  				var nameOfRoom = roomProp.key;
+		  				var numberOfPlayers = childSnapshot.val().numPlayers;
+
+							// find Player and assign room key to the room property
+						  // Make this more efficient by putting code into a function
+						  playersRef.once('value', function(snapshot) {
+						  	snapshot.forEach(function(childSnapshot) {
+						  		if (childSnapshot.val().uid === uid) {
+						  			childSnapshot.ref.update({ 
+								  		room: roomKey 
+								  	});
+						  		}
+						  	})
+						  }).then(function(result){
+						  	roomProp.ref.set(uid);
+						  })
+							
+						  // Now simply find the parent and return the name.
+						  if (numberOfPlayers < 4) {
+						  	childSnapshot.ref.update({ 
+						  		numPlayers: numberOfPlayers + 1 
+						  	});
+						  // Make room locked if there are a total of 4 players
+						  } else if (numberOfPlayers == 4) {
+						  	childSnapshot.ref.update({ locked: true });
+						  }
+
+						  chooseCharacterPageLoad();
+						  
+						 	objData.key = childSnapshot.key;
+						 	dataHasBeenSet = true;
+						}
+						if (dataHasBeenSet) {
+		  				// will exit out of loop
+		  				return true;
+		  			}
+	  		})
+			});
+			return objData;
+		});
+}
+
+function createRoom(uid) {
+	return playersRef.once("value")
+		.then(function(snapshot) {
+			var result = false;
+			snapshot.forEach(function(childSnapshot) {
+  			if (childSnapshot.val().uid === uid) {
+				console.log("We found it!");
+				// currentPlayer = childSnapshot.val();
+				result = true;
+			}
+		});
+		return result;
+		});
+}
+
+function createPlayer(uid, displayName, playerExists) {
+
+  var playerData = {
+    name: displayName,
+    uid: uid,
+    hitPoints: 100, //Default to 100 hitpoints for now
+    turn: 0, // Default to 0 for now. This could be used later with a data ref to turns/
+    playerImage: "", //Currently set to some default image. This will be based off of the character the user chooses.
+    attacks: {
+    	basicAttack: 35,
+    	advancedAttack: 50,
+    	ultraAttack: 75,
+    	ridiculousAttack: 125
+    },
+    items: {},
+    room: null
+  };
+
+  // Get a key for a new Player.
+  var newPlayerKey = playersRef.push().key;
+
+  // Write the new player's data in the players list.
+  var updates = {};
+  updates['/players/' + newPlayerKey] = playerData;
+
+  db.ref().update(updates);
+
+  return playerData;
+}
+
+
+function afterAuth(user) {
+	var userRef = db.ref('/presence/' + user.uid);
+
+  // checks to see if the user is logged in.
+	amOnline.on('value', function(snapshot) {
+	  if (snapshot.val()) {
+	  	// Preferably all users see that user has been disconnected and ill come back.
+	    userRef.onDisconnect().remove();
+
+	    userRef.set(true);
+
+	    // Check to see if player already created
+		  checkIfPlayerExists(user.uid)
+		  	.then(function(arrayOfBooleanAndPlayer){
+
+		  		var playerObj;
+
+		  		// If player doesn't exist, create the player, otherwise mention that player is already in the system
+		  		if (arrayOfBooleanAndPlayer[0] == false) {
+		  			playerObj = createPlayer(user.uid, user.displayName);
+		  		} else if (arrayOfBooleanAndPlayer[0]) {
+		  			playerObj = arrayOfBooleanAndPlayer[1];
+		  		}
+
+		  		console.log(playerObj);
+
+		  		return playerObj;
+
+		  	}).then(function(playerObject){
+		  		// I want the result to be the player that was created
+		  		if (sessionStorage.getItem("characterScreenLoaded") !== 'true') {
+		  			return seatCheckAndRoomUpdate(user.uid, playerObject);
+		  		} else if ((sessionStorage.getItem("characterScreenLoaded") === 'true') && location.href ) {
+
+		  		} else {
+		  			// be sure to return the playerObject so that it gets passed to the next function
+		  			chooseCharacter(user.uid, playerObject);
+		  		}
+		  	}).then(function(result){
+		  			console.log("result from seatCheckAndRoomUpdate", result);
+		  	});
+	  }
+	});
+}
+
+
+var initApp = function() {
+	
+};
+
+// window.addEventListener('load', function() {
+// 	initApp();
+// });
